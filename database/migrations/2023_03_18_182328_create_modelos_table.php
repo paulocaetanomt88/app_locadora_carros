9ef19b6d60d481810d28b2c4ee7a80a556c1fcc2
Paulo Caetano <paulocaetanomt88@gmail.com>
2023-03-18 16:16:36 -0400

    Update: atributos da tabela 'modelos'

--- database/migrations/2023_03_18_182328_create_modelos_table.php
+++ database/migrations/2023_03_18_182328_create_modelos_table.php
@@ -15,7 +15,16 @@ class CreateModelosTable extends Migration
     {
         Schema::create('modelos', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('marca_id');
+            $table->string('nome', 30);
+            $table->string('imagem', 190);
+            $table->integer('lugares');
+            $table->boolean('air_bag');
+            $table->boolean('abs');
             $table->timestamps();
+
+            // chave extrangeira da marca que este modelo pertence (constraint)
+            $table->foreign('marca_id')->references('id')->on('marcas');
         });
     }
 
