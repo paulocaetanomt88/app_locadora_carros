f025bafa6be9ee6e7ba129e78e8908b45b304adf
Paulo E. C. dos Anjos <cast.paulo@mt.sebrae.com.br>
2023-05-05 16:40:28 -0400

    Removendo importações desnecessárias

--- app/Http/Controllers/CarroController.php
+++ app/Http/Controllers/CarroController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Carro;
-use App\Http\Requests\StoreCarroRequest;
-use App\Http\Requests\UpdateCarroRequest;
 use App\Repositories\CarroRepository;
 use Illuminate\Http\Request;
 
