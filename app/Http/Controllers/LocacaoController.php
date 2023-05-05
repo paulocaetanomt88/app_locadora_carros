6048c115a5fb0d8ba2561cc57848ed71db3f374b
Paulo E. C. dos Anjos <cast.paulo@mt.sebrae.com.br>
2023-05-05 17:48:39 -0400

    API WebService Rest para o Resource Locações

--- app/Http/Controllers/LocacaoController.php
+++ app/Http/Controllers/LocacaoController.php
@@ -3,74 +3,128 @@
 namespace App\Http\Controllers;
 
 use App\Models\Locacao;
-use App\Http\Requests\StoreLocacaoRequest;
-use App\Http\Requests\UpdateLocacaoRequest;
+use App\Repositories\LocacaoRepository;
+use Illuminate\Http\Request;
 
 class LocacaoController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    private $locacao;
+
+    public function __construct(Locacao $locacao)
     {
-        //
+        $this->locacao = $locacao;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function index(Request $request)
     {
-        //
+        $locacaoRepository = new LocacaoRepository($this->locacao);
+
+        if ($request->has('atributos_cliente')) {
+            $atributos_cliente = 'cliente:id,' . $request->atributos_cliente;
+
+            $locacaoRepository->selectAtributosRegistrosRelacionados($atributos_cliente);
+        } else {
+            $locacaoRepository->selectAtributosRegistrosRelacionados('cliente');
+        }
+
+        if ($request->has('atributos_carro')) {
+            $atributos_carro = 'carro:id,' . $request->atributos_carro;
+
+            $locacaoRepository->selectAtributosRegistrosRelacionados($atributos_carro);
+        } else {
+            $locacaoRepository->selectAtributosRegistrosRelacionados('carro');
+        }
+
+        if ($request->has('filtro')) {
+            $locacaoRepository->filtro($request->filtro);
+        }
+
+        if ($request->has('atributos')) {
+            $locacaoRepository->selectAtributos($request->atributos);
+        }
+
+        // return response()->json($this->marca->get(), 200);
+        return response()->json($locacaoRepository->getResultado(), 200); 
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreLocacaoRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreLocacaoRequest $request)
+    public function store(Request $request)
     {
-        //
+        $request->validate($this->locacao->rules(), $this->locacao->feedback());
+
+        $locacao = $this->locacao->create([
+            'cliente_id' => $request->cliente_id,
+            'carro_id' => $request->carro_id,
+            'periodo_data_inicio' => $request->periodo_data_inicio,
+            'periodo_data_final_previsto' => $request->periodo_data_final_previsto,
+            'periodo_data_final_realizado' => $request->periodo_data_final_realizado,
+            'valor_diaria' => $request->valor_diaria,
+            'km_inicial' => $request->km_inicial,
+            'km_final' => $request->km_final
+        ]);
+
+        return response()->json($locacao, 201);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Locacao  $locacao
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Locacao $locacao)
+    public function show($id)
     {
-        //
+        $locacao = $this->locacao->with('cliente')->with('carro')->find($id);
+
+        // Controle de fluxo para não mostrar apenas um retorno vazio ou null caso não for encontrado o $id da marca no banco de dados
+        if ($locacao === null) {
+
+            return response()->json(['erro' => 'Recurso não encontrado'], 404); // -> isso retorna: { "erro": "Recurso não encontrado" }
+        }
+
+        return response()->json($locacao, 200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Locacao  $locacao
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Locacao $locacao)
+    public function update(Request $request, $id)
     {
-        //
-    }
+        $locacao = $this->locacao->find($id);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateLocacaoRequest  $request
-     * @param  \App\Models\Locacao  $locacao
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateLocacaoRequest $request, Locacao $locacao)
-    {
-        //
+        if ($locacao === null) {
+            return response()->json(['erro' => 'Impossível realizar a atualização. O recurso solicitado não existe '], 404);
+        }
+
+        if ($request->method() === 'PATCH') {
+            $regrasDinamicas = array();
+
+            // percorrendo todas as regras definidas no Model
+            foreach ($locacao->rules() as $input => $regra) {
+                if (array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+
+            $request->validate($regrasDinamicas, $locacao->feedback());
+
+            // percorrendo os inputs vindos em $request->all()
+            foreach ($request->all() as $input => $valor) {
+
+                if ($input != null && $input != '_method') {
+                    $locacao->update([
+                        $input => $valor,
+                    ]);
+                }
+            }
+        }
+
+        if ($request->method() === 'PUT') {
+            $request->validate($this->locacao->rules(), $locacao->feedback());
+
+            $locacao->update([
+                'cliente_id' => $request->cliente_id,
+                'carro_id' => $request->carro_id,
+                'periodo_data_inicio' => $request->periodo_data_inicio,
+                'periodo_data_final_previsto' => $request->periodo_data_final_previsto,
+                'periodo_data_final_realizado' => $request->periodo_data_final_realizado,
+                'valor_diaria' => $request->valor_diaria,
+                'km_inicial' => $request->km_inicial,
+                'km_final' => $request->km_final
+            ]);
+        }
+
+        return response()->json($locacao, 200);
     }
 
     /**
@@ -79,8 +133,16 @@ class LocacaoController extends Controller
      * @param  \App\Models\Locacao  $locacao
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Locacao $locacao)
+    public function destroy($id)
     {
-        //
+        $locacao = $this->locacao->find($id);
+
+        if ($locacao == null) {
+            return response()->json(['erro' => 'Impossível realizar a exclusão. O recurso não existe.'], 404);
+        }
+
+        $locacao->delete();
+
+        return response()->json(['msg' => 'A locação foi removida com sucesso!'], 200);
     }
 }
--- app/Models/Locacao.php
+++ app/Models/Locacao.php
@@ -10,4 +10,43 @@ class Locacao extends Model
     use HasFactory;
 
     protected $table ='locacoes';
+
+    protected $fillable = [
+        'cliente_id',
+        'carro_id',
+        'periodo_data_inicio',
+        'periodo_data_final_previsto',
+        'periodo_data_final_realizado',
+        'valor_diaria',
+        'km_inicial',
+        'km_final'
+    ];
+
+    public function rules() {
+        return [
+            'cliente_id' => 'exists:clientes,id',
+            'carro_id' => 'exists:carros,id',
+            'periodo_data_inicio' => 'required',
+            'periodo_data_final_previsto' => 'required',
+            'periodo_data_final_realizado' => 'required',
+            'valor_diaria' => 'required',
+            'km_inicial' => 'required',
+            'km_final' => 'required',
+        ];
+    }
+
+    public function feedback() {
+        return [
+            'required' => 'O campo :attribute é obrigatório',
+            'exists' => 'O registro não existe na tabela referenciada'
+        ];
+    }
+
+    public function carro() {
+        return $this->belongsTo('App\Models\Carro');
+    }
+
+    public function cliente() {
+        return $this->belongsTo('App\Models\Cliente');
+    }
 }
