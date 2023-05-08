c36df4c25863e2c55b289723fbac555ea1580ff2
Paulo E. C. dos Anjos <cast.paulo@mt.sebrae.com.br>
2023-05-08 17:32:34 -0400

    Adicionando jwt.auth no kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,5 +62,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'jwt.auth' => \Tymon\JWTAuth\Http\Middleware\Authenticate::class, // 'Tymon\JWTAuth\Http\Middleware\Authenticate' is deprecated.intelephense(1007) ?? Pesquisar isto
     ];
 }
