f2d8988d10cb5f268884bcd98337461caa72bae7
Paulo E. C. dos Anjos <cast.paulo@mt.sebrae.com.br>
2023-05-08 17:33:07 -0400

    implementando o método de login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,21 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login() {
+    public function login(Request $request) {
+        // autenticação (email e senha)
+        // obtendo apenas email e senha de request->all()
+        $credenciais = $request->all(['email', 'password']);
+
+        // $token recebe o retorno que é gerado pela tentativa de autenticação pela api com base nas credenciais passadas
+        $token = auth('api')->attempt($credenciais);
+
+        if ($token) { // usuário autenticado com sucesso
+            return response()->json(['token' => $token]);
+        } else { // erro de usuário ou senha
+            return response()->json(['erro' => 'Usuário ou senha inválido'], 403); // 403 -> forbidden -> proibido
+        }
+        
+        // retornar um Json Web Token
         return 'login';
     }
 
