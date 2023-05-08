e655dad4816f69adb811fb764019f2a3b545bee5
Paulo E. C. dos Anjos <cast.paulo@mt.sebrae.com.br>
2023-05-08 17:34:23 -0400

    Criando grupo de rotas protegidas c/ versionamento

--- routes/api.php
+++ routes/api.php
@@ -19,12 +19,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::resource('cliente', 'App\Http\Controllers\ClienteController');
-Route::apiResource('cliente', 'ClienteController');
-Route::apiResource('carro', 'CarroController');
-Route::apiResource('locacao', 'LocacaoController');
-Route::apiResource('marca', 'MarcaController');
-Route::apiResource('modelo', 'ModeloController');
+Route::prefix('v1')->middleware('jwt.auth')->group(function() {
+    Route::apiResource('cliente', 'ClienteController');
+    Route::apiResource('carro', 'CarroController');
+    Route::apiResource('locacao', 'LocacaoController');
+    Route::apiResource('marca', 'MarcaController');
+    Route::apiResource('modelo', 'ModeloController');
+});
+
 
 Route::post('login', 'AuthController@login');
 Route::post('logout', 'AuthController@logout');
