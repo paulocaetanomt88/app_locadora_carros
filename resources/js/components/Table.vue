7b53a008bdfa62c49c287a8cb80aabd69a0a0eac
Paulo E. C. dos Anjos <cast.paulo@mt.sebrae.com.br>
2023-05-16 17:17:57 -0400

    add botões p/ visualizar atualizar e remover marca

--- resources/js/components/Table.vue
+++ resources/js/components/Table.vue
@@ -4,6 +4,7 @@
       <thead>
         <tr>
           <th scope="col" v-for="t, key in titulos" :key="key" >{{t.titulo}}</th>
+          <th v-if="visualizar || atualizar || remover"></th>
         </tr>
       </thead>
       <tbody>
@@ -15,6 +16,11 @@
                     <img :src="'/storage/'+valor" width="30" height="30">
                 </span>
             </td>
+            <td v-if="visualizar || atualizar || remover">
+                <button v-if="visualizar" class="btn btn-outline-primary btn-sm">Visualizar</button>
+                <button v-if="atualizar" class="btn btn-outline-primary btn-sm">Atualizar</button>
+                <button v-if="remover" class="btn btn-outline-danger btn-sm">Remover</button>
+            </td>
         </tr>
       </tbody>
     </table>
@@ -23,7 +29,7 @@
 
 <script>
 export default {
-  props: ["dados", 'titulos'],
+  props: ["dados", 'titulos', 'visualizar', 'atualizar', 'remover'],
   computed: {
     dadosFiltrados() {
 
