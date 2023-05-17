2938d4be21c032f0511e92c0957d73b17c2a2e2c
Paulo E. C. dos Anjos <cast.paulo@mt.sebrae.com.br>
2023-05-17 10:23:57 -0400

    Install e config o Vuex no projeto

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,17 @@ require('./bootstrap');
 
 window.Vue = require('vue').default;
 
+// Importando e configurando o Vuex
+import Vuex from 'Vuex';
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state: {
+        teste: 'Teste de recuperação de valor da Store do Vuex'
+    }
+})
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -38,4 +49,5 @@ Vue.component('paginate-component', require('./components/Paginate.vue').default
 
 const app = new Vue({
     el: '#app',
+    store
 });
--- resources/js/components/Table.vue
+++ resources/js/components/Table.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    
     <table class="table table-hover">
       <thead>
         <tr>
