84c904c96e055643061199d65ab33717f8244c0b
Paulo E. C. dos Anjos <cast.paulo@mt.sebrae.com.br>
2023-05-17 17:12:02 -0400

    Implementando modal de atualização de marca

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -57,7 +57,7 @@
             <table-component
                 :dados="marcas.data"
                 :visualizar="{ visivel: true, dataToggle: 'modal', dataTarget: '#modalMarcaVisualizar'}"
-                :atualizar="true"
+                :atualizar="{ visivel: true, dataToggle: 'modal', dataTarget: '#modalMarcaAtualizar'}"
                 :remover="{ visivel: true, dataToggle: 'modal', dataTarget: '#modalMarcaRemover'}"
                 :titulos=" {
                     id: {titulo: 'ID', tipo: 'texto'},
@@ -95,10 +95,20 @@
 
     <!-- Início do modal de inclusão de marca  -->
     <modal-component id="modalMarca" titulo="Adicionar marca">
-
       <template v-slot:alertas>
-        <alert-component tipo="success" :detalhes="transacaoDetalhes" titulo="A marca foi cadastrada com sucesso." v-if="transacaoStatus == 'adicionado'"></alert-component>
-        <alert-component tipo="danger" :detalhes="transacaoDetalhes" titulo="Erro ao tentar cadastrar a marca" v-if="transacaoStatus == 'erro'"></alert-component>
+        <alert-component
+            tipo="success" 
+            :detalhes="transacaoDetalhes" 
+            titulo="A marca foi cadastrada com sucesso." 
+            v-if="transacaoStatus == 'adicionado'">
+        </alert-component>
+
+        <alert-component 
+            tipo="danger" 
+            :detalhes="transacaoDetalhes" 
+            titulo="Erro ao tentar cadastrar a marca" 
+            v-if="transacaoStatus == 'erro'">
+        </alert-component>
       </template>
 
       <template v-slot:conteudo>
@@ -166,6 +176,66 @@
         </template>
     </modal-component>
     <!-- Fim do modal de visualização de marca  -->
+
+    <!-- Início do modal de atualização de marca  -->
+    <modal-component id="modalMarcaAtualizar" titulo="Atualizar marca">
+      <template v-slot:alertas>
+        <alert-component 
+                tipo="success"
+                titulo="Transação realizada com sucesso"
+                :detalhes="$store.state.transacao" 
+                v-if="$store.state.transacao.status == 'sucesso'"
+            ></alert-component>
+            <alert-component
+                tipo="danger"
+                titulo="Erro na transação"
+                :detalhes="$store.state.transacao"
+                v-if="$store.state.transacao.status == 'erro'"
+            ></alert-component>
+      </template>
+
+      <template v-slot:conteudo>
+      <div class="form-group">
+        <input-container-component
+          titulo="Nome da marca"
+          id="atualizarNome"
+          id-help="atualizarNomeHelp"
+          texto-ajuda="Informe o nome da marca"
+        >
+          <input
+            type="text"
+            class="form-control"
+            id="atualizarNome"
+            aria-describedby="atualizarNomeHelp"
+            placeholder="Nome da marca"
+            v-model="$store.state.item.nome"
+          />
+        </input-container-component>
+      </div>
+      <div class="form-group">
+        <input-container-component
+          titulo="Imagem"
+          id="atualizarImagem"
+          id-help="atualizarImagemHelp"
+          texto-ajuda="Selecione uma imagem no formato PNG"
+        >
+          <input
+            type="file"
+            class="form-control-file"
+            id="atualizarImagem"
+            aria-describedby="atualizarImagemHelp"
+            placeholder="Selecione uma imagem"
+            @change="carregarImagem($event)"
+          />
+        </input-container-component>
+      </div>
+      </template>
+      <template v-slot:rodape>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+        <button type="button" class="btn btn-primary" @click="atualizar()">Atualizar</button>
+      </template>
+    </modal-component>
+    <!-- Fim do modal de atualização de marca  -->
 
     <!-- Início do modal de remoção de marca  -->
     <modal-component id="modalMarcaRemover" titulo="Remover marca">
@@ -235,6 +305,33 @@ export default {
     };
   },
   methods: {
+    atualizar() {
+        let formData = new FormData();
+        formData.append('_method', 'patch')
+        formData.append('nome', this.$store.state.item.nome)
+        formData.append('imagem', this.arquivoImagem[0])
+
+        let config = {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                'Accept': 'application/json',
+                'Authorization': this.token
+            }
+        }
+
+        let url = this.urlBase + '/' + this.$store.state.item.id
+
+        axios.post(url, formData, config)
+            .then(response => {
+                this.$store.state.transacao.status = 'sucesso'
+                this.$store.state.transacao.mensagem = response.data.msg
+                this.carregarLista()
+            })
+            .catch(errors => {
+                this.$store.state.transacao.status = 'erro'
+                this.$store.state.transacao.mensagem = errors.response.data.erro
+            })
+    },
     remover() {
         let confirmacao = confirm('Tem certeza que deseja remover esse registro?')
 
--- resources/js/components/Table.vue
+++ resources/js/components/Table.vue
@@ -5,7 +5,7 @@
       <thead>
         <tr>
           <th scope="col" v-for="t, key in titulos" :key="key" >{{t.titulo}}</th>
-          <th v-if="visualizar.visivel || atualizar || remover.visivel"></th>
+          <th v-if="visualizar.visivel || atualizar.visivel || remover.visivel"></th>
         </tr>
       </thead>
       <tbody>
@@ -17,9 +17,9 @@
                     <img :src="'/storage/'+valor" width="30" height="30">
                 </span>
             </td>
-            <td v-if="visualizar.visivel || atualizar || remover.visivel">
+            <td v-if="visualizar.visivel || atualizar.visivel || remover.visivel">
                 <button v-if="visualizar.visivel" class="btn btn-outline-primary btn-sm" :data-toggle="visualizar.dataToggle" :data-target="visualizar.dataTarget" @click="setStore(obj)">Visualizar</button>
-                <button v-if="atualizar" class="btn btn-outline-primary btn-sm">Atualizar</button>
+                <button v-if="atualizar.visivel" class="btn btn-outline-primary btn-sm" :data-toggle="atualizar.dataToggle" :data-target="atualizar.dataTarget" @click="setStore(obj)">Atualizar</button>
                 <button v-if="remover.visivel" class="btn btn-outline-danger btn-sm" :data-toggle="remover.dataToggle" :data-target="remover.dataTarget" @click="setStore(obj)">Remover</button>
             </td>
         </tr>
